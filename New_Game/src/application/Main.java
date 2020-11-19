a165ef83c962e5b39df4bc8a2ce491b9af7dfa93
ddhroov10 <dhroov19303@iiitd.ac.in>
2020-11-20 01:11:24 +0530

    4th obstacle added

--- New_Game/src/application/Main.java
+++ New_Game/src/application/Main.java
@@ -145,16 +145,31 @@ public class Main extends Application {
 			arr_rotate.get(i).setAngle(arr_rotate.get(i).getAngle()+1);
 		}
 		for (int i = 0; i < arr_hrzntl_rotate.size(); i++) {
-			for (int j = 0; j < arr_hrzntl_rotate.get(i).getList_shape().size(); j++) {
+//			for (int j = 0; j < arr_hrzntl_rotate.get(i).getList_shape().size(); j++) {
 //				(arr_hrzntl_rotate.get(i).getList_shape().get(j)).setLayoutX(arr_hrzntl_rotate.get(i).getList_shape().get(j).getLayoutX()+.5);
-				if(!hrzntl_mov) {
-					(arr_hrzntl_rotate.get(i).getList_shape().get(j)).setLayoutX(arr_hrzntl_rotate.get(i).getList_shape().get(j).getLayoutX()+.5);
+				if(arr_hrzntl_rotate.get(i).getList_shape().get(0).getLayoutX()>=-300 && hrzntl_mov==false){
+					for (int j = 0; j < arr_hrzntl_rotate.get(i).getList_shape().size(); j++) {
+						(arr_hrzntl_rotate.get(i).getList_shape().get(j)).setLayoutX(arr_hrzntl_rotate.get(i).getList_shape().get(j).getLayoutX()-1);
+					}
+					if(arr_hrzntl_rotate.get(i).getList_shape().get(0).getLayoutX()==-300)hrzntl_mov=true;
+					
 				}
+				else if(hrzntl_mov==true){
+					for (int j = 0; j < arr_hrzntl_rotate.get(i).getList_shape().size(); j++) {
+						(arr_hrzntl_rotate.get(i).getList_shape().get(j)).setLayoutX(arr_hrzntl_rotate.get(i).getList_shape().get(j).getLayoutX()+1);
+					}
+					if(arr_hrzntl_rotate.get(i).getList_shape().get(0).getLayoutX()>=300) {
+						hrzntl_mov=false;
+					}
+				}
+//				if(!hrzntl_mov) {
+//					(arr_hrzntl_rotate.get(i).getList_shape().get(j)).setLayoutX(arr_hrzntl_rotate.get(i).getList_shape().get(j).getLayoutX()+.5);
+//				}
 //				else {
 //					(arr_hrzntl_rotate.get(i).getList_shape().get(j)).setLayoutX(arr_hrzntl_rotate.get(i).getList_shape().get(j).getLayoutX()-1);
 //				}
 //				hrzntl_mov=!hrzntl_mov;
-			}
+//			}
 //			for (int j = 0; j < 4; j++) {
 //				(arr_hrzntl_rotate.get(i).getList_shape().get(j)).setLayoutX(arr_hrzntl_rotate.get(i).getList_shape().get(j).getLayoutX()-1);
 //			}
@@ -162,6 +177,9 @@ public class Main extends Application {
 		}
 		if(circle.getLayoutY()<frm_height/2) {
 			screen_mover=true;
+		}
+		else {
+			screen_mover=false;
 		}
 		
 		for (int i = 0; i < Obstacles.size(); i++) {
@@ -208,13 +226,13 @@ public class Main extends Application {
 		if (gameup==1) {
 			circle.setLayoutY(circle.getLayoutY() -7);
 			
-			if(screen_mover) {
+			if(screen_mover ) {
 				for (int i = 0; i < Obstacles.size(); i++) {
 					for (int j = 0; j < Obstacles.get(i).getList_shape().size(); j++) {
-					Obstacles.get(i).getList_shape().get(j).setLayoutY(Obstacles.get(i).getList_shape().get(j).getLayoutY()+3);
-				
+						Obstacles.get(i).getList_shape().get(j).setLayoutY(Obstacles.get(i).getList_shape().get(j).getLayoutY()+3);
 					}
-				}	
+				}
+				frm_height+=3;
 			}
 		}
 		
